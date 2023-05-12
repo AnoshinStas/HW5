7cdc1160d082bf10e7642120f1f57e05823840b2
Stanislav <anoshin.stanislav@yandex.ru>
2023-05-12 13:18:21 +0500

    HW5.3

---
+++ HW5.3/Program.cs
@@ -0,0 +1,40 @@
+﻿void FillArray(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+
+void PrintArray(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите размер массива  ");
+int size = int.Parse(Console.ReadLine());
+double[] numbers = new double[size];
+FillArray(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int x = 0; x < numbers.Length; x++)
+{
+    if (numbers[x] > max)
+        {
+            max = numbers[x];
+        }
+    if (numbers[x] < min)
+        {
+            min = numbers[x];
+        }
+}
+
+Console.WriteLine($"всего чисел {numbers.Length}. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
