2f9dee1034695397b2c10351c970fc9cc90b13f2
Stanislav <anoshin.stanislav@yandex.ru>
2023-05-12 11:56:16 +0500

    HW5.1

---
+++ HW5.1/Program.cs
@@ -0,0 +1,31 @@
+﻿void FillArray(int[] numbers)
+{
+    Random rmd = new Random();
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = rmd.Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();    
+}
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+int[] numbers = new int[size];
+FillArray(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int x = 0; x < numbers.Length; x++)
+if (numbers[x] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего чисел {numbers.Length}, чётных {count}");
